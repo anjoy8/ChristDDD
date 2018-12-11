01d4619a907e62cc25ed3f790e5a8da2e3799f4f
anjoy8 <3143422472@qq.com>
2018-12-11 22:33:51 +0800

    Modify part of the structure

---
+++ Christ3D.Infrastruct.Data/Bus/InMemoryBus.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Christ3D.Domain.Core.Bus;
+using Christ3D.Domain.Core.Commands;
+using Christ3D.Domain.Core.Events;
+using MediatR;
+using MediatR.Internal;
+
+namespace Christ3D.Infra.Bus
+{
+    /// <summary>
+    /// 一个密封类，实现我们的中介内存总线
+    /// </summary>
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        //构造函数注入
+        private readonly IMediator _mediator;
+        //注入服务工厂
+        private readonly ServiceFactory _serviceFactory;
+        private static readonly ConcurrentDictionary<Type, object> _requestHandlers = new ConcurrentDictionary<Type, object>();
+        // 事件仓储服务
+        private readonly IEventStoreService _eventStoreService;
+
+
+        public InMemoryBus(IMediator mediator, ServiceFactory serviceFactory,IEventStoreService eventStoreService)
+        {
+            _mediator = mediator;
+            _serviceFactory = serviceFactory;
+            _eventStoreService = eventStoreService;
+        }
+
+        /// <summary>
+        /// 实现我们在IMediatorHandler中定义的接口
+        /// 没有返回值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            //这个是正确的
+            return _mediator.Send(command);//请注意 入参 的类型
+
+            //注意！这个仅仅是用来测试和研究源码的，请开发的时候不要使用这个
+            //return Send(command);//请注意 入参 的类型
+        }
+
+        /// <summary>
+        /// 引发事件的实现方法
+        /// </summary>
+        /// <typeparam name="T">泛型 继承 Event：INotification</typeparam>
+        /// <param name="event">事件模型，比如StudentRegisteredEvent</param>
+        /// <returns></returns>
+        public Task RaiseEvent<T>(T @event) where T : Event
+        {
+            // 除了领域通知以外的事件都保存下来
+            if (!@event.MessageType.Equals("DomainNotification"))
+                _eventStoreService?.Save(@event);
+
+            // MediatR中介者模式中的第二种方法，发布/订阅模式
+            return _mediator.Publish(@event);
+        }
+
+      
+    }
+}
---
+++ Christ3D.UI.Web/Extensions/NativeInjectorBootStrapper.cs
@@ -0,0 +1,72 @@
+﻿using Christ3D.Application.EventSourcing;
+using Christ3D.Application.Interfaces;
+using Christ3D.Application.Services;
+using Christ3D.Domain.CommandHandlers;
+using Christ3D.Domain.Commands;
+using Christ3D.Domain.Core.Bus;
+using Christ3D.Domain.Core.Events;
+using Christ3D.Domain.Core.Notifications;
+using Christ3D.Domain.EventHandlers;
+using Christ3D.Domain.Events;
+using Christ3D.Domain.Interfaces;
+using Christ3D.Infra.Bus;
+using Christ3D.Infra.Data.Context;
+using Christ3D.Infra.Data.Repository;
+using Christ3D.Infra.Data.Repository.EventSourcing;
+using Christ3D.Infra.Data.UoW;
+using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Christ3D.Infra.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // 注入 应用层Application
+            services.AddScoped<IStudentAppService, StudentAppService>();
+
+            // 命令总线Domain Bus (Mediator)
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+
+            // Domain - Events
+            // 将事件模型和事件处理程序匹配注入
+
+            // 领域通知
+            services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            // 领域事件
+            services.AddScoped<INotificationHandler<StudentRegisteredEvent>, StudentEventHandler>();
+            services.AddScoped<INotificationHandler<StudentUpdatedEvent>, StudentEventHandler>();
+            services.AddScoped<INotificationHandler<StudentRemovedEvent>, StudentEventHandler>();
+
+
+            // Domain - Commands
+            // 将命令模型和命令处理程序匹配
+            services.AddScoped<IRequestHandler<RegisterStudentCommand, Unit>, StudentCommandHandler>();
+            services.AddScoped<IRequestHandler<UpdateStudentCommand, Unit>, StudentCommandHandler>();
+            services.AddScoped<IRequestHandler<RemoveStudentCommand, Unit>, StudentCommandHandler>();
+
+            // Domain - Memory
+            services.AddSingleton<IMemoryCache>(factory =>
+            {
+                var cache = new MemoryCache(new MemoryCacheOptions());
+                return cache;
+            });
+
+
+
+            // 注入 基础设施层 - 数据层
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<StudyContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            // 注入 基础设施层 - 事件溯源
+            services.AddScoped<IEventStoreRepository, EventStoreSQLRepository>();
+            services.AddScoped<IEventStoreService, SqlEventStoreService>();
+            services.AddScoped<EventStoreSQLContext>();
+        }
+    }
+}
