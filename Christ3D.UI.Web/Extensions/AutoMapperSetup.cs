7ed3277e981a2e311613310086298f895b5b216e
anjoy <3143422472@qq.com>
2019-10-15 17:44:17 +0800

    Upgrade nuget package and solve the automapper error
    升级nuget包，并解决automapper因为升级后，出现的二义性错误

--- Christ3D.UI.Web/Extensions/AutoMapperSetup.cs
+++ Christ3D.UI.Web/Extensions/AutoMapperSetup.cs
@@ -14,8 +14,11 @@ namespace Christ3D.UI.Web.Extensions
         public static void AddAutoMapperSetup(this IServiceCollection services)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
-            //添加服务
-            services.AddAutoMapper();
+            //添加服务,如果Profile文件在 api 层，这里，可以直接用 typeof(Startup) 就行
+            //但是如果写到了其他层，必须写 config,
+            //AutoMapper会从程序集中搜索Profile的子类，然后把这些子类加入到配置中。
+            //如果Profile文件是在当前程序中定义，那没有问题，但如果是写在外部类库内，则是搜索不到的
+            services.AddAutoMapper(typeof(AutoMapperConfig));
             //启动配置
             AutoMapperConfig.RegisterMappings();
         }
