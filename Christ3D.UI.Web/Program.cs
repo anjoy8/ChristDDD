946ef27381cd6ecb6617b1404525ae7780311617
anjoy8 <3143422472@qq.com>
2020-05-15 00:27:35 +0800

    config port

--- Christ3D.UI.Web/Program.cs
+++ Christ3D.UI.Web/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Christ3D.UI.Web
@@ -14,11 +15,16 @@ namespace Christ3D.UI.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder =>
+          {
+              webBuilder
+              .UseStartup<Startup>();
+          });
     }
 }
